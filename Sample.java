4b61b4495dbd9a69afb00062e35be3cc9be435c0
Anand <yannamanenianandkumar@gmail.com>
2024-07-22 12:49:50 -0400

    Completed Competitive-Coding-5

--- Sample.java
+++ Sample.java
@@ -5,3 +5,76 @@
 
 
 // Your code here along with comments explaining your approach
+//----------------------------------------------
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+
+
+        List<Integer> result = new ArrayList<>();
+        if (root==null) return result;
+        Queue<TreeNode> q = new LinkedList<>();
+
+        q.add(root);
+
+        while(!q.isEmpty()){
+            int max = Integer.MIN_VALUE;
+            int size = q.size();
+
+            for(int i=0;i<size;i++){
+            TreeNode curr = q.poll();
+            max = Math.max(max,curr.val);
+            if(curr.left!=null){
+                q.add(curr.left);
+            }
+            if(curr.right!=null){
+                q.add(curr.right);
+            }
+            }
+            result.add(max);    
+        }
+        return result;
+    }
+}
+//----------------------------------------------------
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        int N = 9;
+
+        // Use an array to record the status
+        int[][] rows = new int[N][N];
+        int[][] cols = new int[N][N];
+        int[][] boxes = new int[N][N];
+
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+                // Check if the position is filled with number
+                if (board[r][c] == '.') {
+                    continue;
+                }
+                int pos = board[r][c] - '1';
+
+                // Check the row
+                if (rows[r][pos] == 1) {
+                    return false;
+                }
+                rows[r][pos] = 1;
+
+                // Check the column
+                if (cols[c][pos] == 1) {
+                    return false;
+                }
+                cols[c][pos] = 1;
+
+                // Check the box
+                int idx = (r / 3) * 3 + c / 3;
+                if (boxes[idx][pos] == 1) {
+                    return false;
+                }
+                boxes[idx][pos] = 1;
+            }
+        }
+        return true;
+    }
+}
+
+//----------------------------
